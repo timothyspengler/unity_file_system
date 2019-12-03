516f595b52c148d3e2dfda332d8efd9b08b93d95
timothyspengler <timothy.spengler.571@my.csun.edu>
2019-12-02 22:19:10 -0800

    file system functionality

---
+++ Assets/MoveCamera.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    // how fast you move
+    private int movementspeed;
+
+    void Start() {
+        movementspeed = 25;
+    }
+
+    void Update() {
+        // move left
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(Vector3.left * movementspeed * Time.deltaTime);
+
+        // move right
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * movementspeed * Time.deltaTime);
+
+        // move forward
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * movementspeed * Time.deltaTime);
+
+        // move backwards
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * movementspeed * Time.deltaTime);
+
+        // move up
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.Translate(Vector3.up * movementspeed * Time.deltaTime);
+
+        // move down
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.Translate(Vector3.down * movementspeed * Time.deltaTime);
+
+        // rotate right
+        if (Input.GetKey(KeyCode.E))
+            transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * 90f);
+
+        // rotate left
+        if (Input.GetKey(KeyCode.Q))
+            transform.RotateAround(transform.position, Vector3.up, Time.deltaTime * -90f);
+
+        // rotate downward
+        if (Input.GetKey(KeyCode.R))
+            transform.RotateAround(transform.position, Vector3.left, Time.deltaTime * 90f);
+
+        // rotate upward
+        if (Input.GetKey(KeyCode.D))
+            transform.RotateAround(transform.position, Vector3.left, Time.deltaTime * -90f);
+    }
+}
--- Assets/spawner.cs
+++ Assets/spawner.cs
@@ -1,7 +1,11 @@
 ﻿/* 
-    Timothy Spengler
-    Comp 585 -- GUI
-    11/16/19
+    Spawner spawns all objects of the file System which includes files, folders,
+    and drives. Start() begins the file system at Drives, and the user can iteriate
+    through directies displaying all files and folders that are displayed.
+
+    whatToSpawnPrefab[2] = Drives Prefab(capsule) 
+    whatToSpawnPrefab[1] = Folders Prefab(cubes) 
+    whatToSPawnPrefab[0] = Files Prefab(spheres)
  */
 
 using System.Collections;
@@ -10,62 +14,46 @@ using System.IO;
 using System.Linq;
 using System;
 using UnityEngine;
+using System.Security.AccessControl;
 
 
 public class spawner : MonoBehaviour
 {
-    // Global Variables
-    public GameObject[] whatToSpawnPrefab;
- 
+    public GameObject[] whatToSpawnPrefab; // Set in Unity IDE
     public Transform spawnPos;  // position of object
-    //DataNode dn;
 
-    // Platform Dimensions 
-    int grid;
-    float myXPos; // tracks X position when spawning
-    float staticX;
-    float myZPos;  // tracks Y position when spawning
-    float maxLength;
-    public int directorySize;
-    // Must skip F: Drive because it's a DVD drive and it doesn't like that
-    void Start() 
-    {
+    // Variables for Platform Dimensions 
+    public float myXPos;    // tracks X position when spawning
+    float staticX;          // set in SetSpawnDimensions
+    float myZPos;           // tracks Y position when spawning
+    float maxLength;        // set in SetSpawnDimensions
+
+    void Start() {
         SetSpawnDimensions(DriveInfo.GetDrives().Length);
         int track = 1;
         foreach (var drive in DriveInfo.GetDrives()) {
+            // Must skip F: Drive because it's a DVD drive and Tim's computer doesn't like that
             if (drive.Name != "F:\\") 
                 SpawnDriveObjects(drive, track++);
         }
-
-        // -- PROFESSORS CODE END -- 
-    }
-
-    // probably will need. 
-    void Update() 
-    {
     }
 
     // pass Game Object and Drive information
-   public void SpawnDriveObjects(DriveInfo drive, int index) 
-    {
+   public void SpawnDriveObjects(DriveInfo drive, int index) {
         int y = 1;
-        //int z = -4;
-        var gObj = Instantiate(whatToSpawnPrefab[0], new Vector3(myXPos, y, myZPos), spawnPos.rotation) as GameObject;
+        GameObject gObj = Instantiate(whatToSpawnPrefab[2], new Vector3(myXPos, y, myZPos), Quaternion.identity) as GameObject;
         myXPos += 2; 
 
         // reset x and change z position
-        
         if((index) % maxLength == 0) 
         {
             myXPos = staticX;
             myZPos += 2;
         }
 
-        // Position the game object in world space
-        //gObj.transform.position = new Vector3(x, y, z);
-        gObj.name = drive.Name;
-
         // Add DataNode component and update the attributes for later usage
+        gObj.name = drive.Name;
+        gObj.GetComponentInChildren<TextMesh>().text = drive.Name;
         gObj.AddComponent<DataNode>();
         gObj.AddComponent<spawner>();
         DataNode dn = gObj.GetComponent<DataNode>();
@@ -73,72 +61,61 @@ public class spawner : MonoBehaviour
         dn.Size = drive.TotalSize;
         dn.FullName = drive.RootDirectory.FullName;
         dn.IsDrive = true;
-        //dn.Folders = new DirectoryInfo(dn.FullName);
-        dn.Folders = drive.RootDirectory.GetDirectories();
-        dn.Files = drive.RootDirectory.GetFiles();
+        dn.IsFolder = true;
         dn.spawnPos = spawnPos;
-
-        dn.Prefab = whatToSpawnPrefab[0];
+        dn.Prefab = whatToSpawnPrefab;
+        dn.yPos = y;
+        dn.UserHasAccess = true;
     }
 
-    public void SpawnFolderObjects(DirectoryInfo dir, int index, GameObject Prefab) {
-      /*  Debug.Log("FolderSpawnObject - "+dir.FullName);
+    // Sets and spawns all folder game objects
+    public void SpawnFolderObjects(DirectoryInfo dir, int index, GameObject[] Prefab, int oldY, string prevDirectory) {
+        Debug.Log("FolderSpawnObject - "+dir.FullName);
         Debug.Log(dir.FullName);
-        int y = 2;
-        var gObj =Instantiate(Prefab, new Vector3(myXPos, y, myZPos), spawnPos.rotation);
-        if ((index) % maxLength == 0) {
-            myXPos = staticX;
-            myZPos += 2;
-        }
-        /*  int y = 1;
-          //int z = -4;
-          var gObj = Instantiate(whatToSpawnPrefab[0], new Vector3(myXPos, y, myZPos), spawnPos.rotation) as GameObject;
-          myXPos += 2;
-
-          // reset x and change z position
-
-          if ((index) % maxLength == 0) {
-              myXPos = staticX;
-              myZPos += 2;
-          }
-
-          // Position the game object in world space
-          //gObj.transform.position = new Vector3(x, y, z);
-          gObj.name = dir.Name;
-
-          // Add DataNode component and update the attributes for later usage
-          gObj.AddComponent<DataNode>();
-          DataNode dn = gObj.GetComponent<DataNode>();
-          dn.Name = dir.Name;
-          //dn.Size = dir;
-          dn.FullName = dir.FullName;
-          dn.IsDrive = false;
-          dn.IsFolder = true;
-                  dn.spawnPos = spawnPos;
-
-          */
-    }
-
-    public void SpawnFileObjects(FileInfo file, int index, GameObject Prefab) {
-        Debug.Log("FileSpawnObject - "+file.FullName);
-        int y = 3;
-        //int z = -4;
-        var gObj = Instantiate(Prefab, new Vector3(myXPos, y, myZPos), spawnPos.rotation) as GameObject;
+        int y = ToggleY(oldY);
+        var gObj =Instantiate(Prefab[2], new Vector3(myXPos, y, myZPos), spawnPos.rotation);
+      
         myXPos += 2;
-
         // reset x and change z position
-
         if ((index) % maxLength == 0) {
             myXPos = staticX;
             myZPos += 2;
         }
-
-        // Position the game object in world space
-        //gObj.transform.position = new Vector3(x, y, z);
-        gObj.name = file.Name;
 
         // Add DataNode component and update the attributes for later usage
+        gObj.name = dir.Name;
+        gObj.GetComponentInChildren<TextMesh>().text = ShortenString(dir.Name, 14); 
         gObj.AddComponent<DataNode>();
+        DataNode dn = gObj.GetComponent<DataNode>();
+        dn.Name = dir.Name;
+        dn.FullName = dir.FullName;
+        dn.IsDrive = false;
+        dn.IsFolder = true;
+        dn.spawnPos = spawnPos;
+        dn.yPos = y;
+        dn.PrevDirectory = prevDirectory;
+        dn.Prefab = Prefab;
+        dn.UserHasAccess = true;
+    }
+
+    // Sets and spawns all file game objects
+    public void SpawnFileObjects(FileInfo file, int index, GameObject Prefab, int oldY, string prevDirectory) {
+        Debug.Log("FileSpawnObject - " + file.FullName);
+        int y = ToggleY(oldY) ;
+        var gObj = Instantiate(Prefab, new Vector3(myXPos, y, myZPos), spawnPos.rotation) as GameObject;
+    
+        myXPos += 2;
+        // reset x and change z position
+        if ((index) % maxLength == 0) {
+            myXPos = staticX;
+            myZPos += 2;
+        }
+
+        // Add DataNode component and update the attributes for later usage
+        gObj.name = file.Name;
+        //gObj.GetComponentInChildren<TextMesh>().text = dir.Name;
+        gObj.AddComponent<DataNode>();
+   
         DataNode dn = gObj.GetComponent<DataNode>();
         dn.Name = file.Name;
         dn.Size = file.Length;
@@ -146,56 +123,27 @@ public class spawner : MonoBehaviour
         dn.IsDrive = false;
         dn.IsFolder = false;
         dn.spawnPos = spawnPos;
-        //dn.Folders = new DirectoryInfo(dn.FullName);
-       // dn.Folders = drive.RootDirectory.GetDirectories();
-        //dn.Files = drive.RootDirectory.GetFiles();
+        dn.yPos = y;
+        dn.PrevDirectory = prevDirectory;
+        dn.UserHasAccess = true; 
     }
-
- 
 
     // Generates a platform depending on the amount of items in the directory 
     public void SetSpawnDimensions(int length) {
         float x = FindNearestSquare(length);
         float z = x;
-        //float y = .2f;  // independent to x and z
-
-        myXPos = (x / -2) * 1f;      // for placing objects
-        maxLength = (x * 1f) ;    // for placing objects
-       // Debug.Log(maxLength);
-        staticX = myXPos;       // for placing objects
-        myZPos = (x / -2) ;      // for placing objects
-
-        // Create Platform and set position and dimensions. 
-      //  GameObject platform = GameObject.CreatePrimitive(PrimitiveType.Cube);
-
-       // platform.transform.position = new Vector3(0, 0, 0);
-        //platform.transform.localScale = new Vector3(x * 2f, y, x * 2f);
-
-    }
-
-    public void SpawnNextDirectory(string docPath) {
-        Debug.Log(docPath +"YEAH");
-        DirectoryInfo dirs = new DirectoryInfo(docPath);
-        Debug.Log(dirs.FullName);
-        SetSpawnDimensions(dirs.GetDirectories().Length + dirs.GetFiles().Length);
-        int index = 1;
-
-         //Spawn Folders
-        foreach(var dir in dirs.EnumerateDirectories()) {
-         //  SpawnFolderObjects(dir, index++);
-        }
-
-        // Spawn Files
-       // foreach(var file in dirs.EnumerateFiles()) {
-       //    SpawnFileObjects(file, index++);
-       // }
+        
+        // Set global variables
+        myXPos = (x / -2) * 1f;
+        maxLength = (x * 1f) ;    
+        staticX = myXPos;       
+        myZPos = (x / -2) ;      
     }
 
     /*  Function will find the closest square root to an integer 
         that is less than or equal to n
     */
     private int FindNearestSquare(int n) {
-        
         int square = 4; //smallest square we will use
         int odd = 5;
 
@@ -204,11 +152,27 @@ public class spawner : MonoBehaviour
                 odd += 2;
       
         }
-       // square += odd; //needs this to prevent objects from falling
-        //return square;
         return Convert.ToInt32(Math.Sqrt(square));
     }
 
- 
+    /*  Toggle the Y position of the game objects. Y positions 
+        alternate when moving directories to know which game objects
+        to remove from display. 
+    */
+    private int ToggleY(int y) {
+        if(y == 1)
+            y = 2;
+        else
+            y = 1;
 
+        return y;
+    }
+
+    // Shortens names if longer than the max number of characters
+    private string ShortenString(string name, int max) {
+        return name.Length <= max ? name : name.Substring(0, max) + "...";
+    }
+
+   
+ 
 } // end class
